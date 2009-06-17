42120b8cf5a5cc49c9e2092bc66855df4e9354f1
Tom Lokhorst <tom@lokhorst.eu>
2009-06-17 08:50:28 +0200

    Added CsPrelude, empty still.

---
+++ src/CsPrelude.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE GADTs, EmptyDataDecls, FlexibleInstances, MultiParamTypeClasses
+           , UndecidableInstances #-}
+
+module CsPrelude where
+
+import AwesomePrelude
+import qualified Prelude as P
+import Prelude hiding (Maybe, Either, Bool, Eq, (==), (&&), (++))
+import Data.List
+
+
