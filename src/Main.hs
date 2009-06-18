ef4abad7352ca1a0676f05c0c7d27ef314010aca
Tom Lokhorst <tom@lokhorst.eu>
2009-06-18 16:23:51 +0200

    Example of how partial application fails (see `epicfail`)

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE NoImplicitPrelude, RankNTypes #-}
 
 module Main where
 
@@ -7,12 +7,23 @@ import HaskellPrelude
 import JsPrelude
 
 import Prelude (Int, fromInteger)
+import qualified Prelude as P
 
 testy :: [Int]
-testy = (3 :: Int) `cons` (4 `cons` nil)
+testy = 3 `cons` (4 `cons` nil)
 
+-- test :: P.Bool
 test :: Js JsBool
 test = not false && false || true
+
+if' :: a -> a -> P.Bool -> a
+if' x y b = if b then x else y
+
+f :: (forall a. a -> a -> b -> a) -> a -> b -> a
+f g a b = g a a b
+
+ok = f if'
+-- epicfail = f bool
 
 -- hoi = maybee false (not_) (just true :: JsC1 JsMaybe (Js JsBool))
 
