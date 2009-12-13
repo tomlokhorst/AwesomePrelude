6d0eaad804bac3cedf29306312981ee6b17e7cfb
Sebastiaan Visser <sfvisser@cs.uu.nl>
2009-12-13 14:47:09 +0100

    Indenting compiled functions.

--- src/Compiler/Compiler.hs
+++ src/Compiler/Compiler.hs
@@ -14,8 +14,11 @@ workerJs = foldVal
   (\_ r0 r1 i f a -> concat r0 ++ "\n" ++ concat r1 ++ "\n"
                   ++ mkAssign i ++ mkId f ++ "(" ++ mkId a ++ ")")
   (\_       i s   -> mkAssign i ++ s)
-  (\_ r0    i v b -> mkAssign i ++ "function (" ++ v ++ ") {\n" ++ concat r0 ++ "\n return " ++ mkId b ++ "}")
+  (\_ r0    i v b -> mkAssign i ++ "function (" ++ v ++ ")\n{\n" ++ indent (concat r0) ++ "  return " ++ mkId b ++ "\n}")
   (\_       i v   -> mkAssign i ++  v)
+
+indent :: String -> String
+indent = unlines . map ("  "++) . lines
 
 mkId :: Int -> String
 mkId i = '_':show i
