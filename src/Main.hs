c06fc73e1489aecdd3556b0395d427065245efee
Tom Lokhorst <tom@lokhorst.eu>
2011-09-20 18:56:45 +0200

    Made Main.hs compile again

--- src/Main.hs
+++ src/Main.hs
@@ -5,11 +5,14 @@ import Generic.Prelude
 import Lang.JavaScript
 import qualified Prelude as P
 
-jsSumList :: JavaScript Num
-jsSumList = sum (replicate 3 (2 * 8) ++ replicate 3 8) * maybe 4 (*8) (just (3 - 2))
+three :: JavaScript Number
+three = 3
+
+jsSumList :: JavaScript Number
+jsSumList = sum (replicate three (2 * 8) ++ replicate three 8) * maybe 4 (*8) (just (3 - 2))
 
 main :: P.IO ()
-main =
-  do out <- compiler jsSumList
-     P.writeFile "test.js" out
+main = do
+  out <- compiler jsSumList
+  P.writeFile "test.js" out
 
